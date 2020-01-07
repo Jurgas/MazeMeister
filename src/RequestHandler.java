ecf9075b353aa66fcbc9931c281e928bb6fb951a
Jurgas <sebastianjurga7@gmail.com>
2020-01-07 16:51:38 +0100

    add new requests

--- src/RequestHandler.java
+++ src/RequestHandler.java
@@ -9,39 +9,75 @@ import java.nio.file.Paths;
 public class RequestHandler {
 
 
-    public void getPoss(String uid, String map) {
+    public Object[] getPoss(String uid, String map) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://tesla.iem.pw.edu.pl:4444/" + uid + "/" + map + "/possibilities"))
                 .build();
 
-        response(client, request);
+        return response(client, request);
     }
 
-    public void move(String uid, String map, String des) {
+    public Object[] move(String uid, String map, String des) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://tesla.iem.pw.edu.pl:4444/" + uid + "/" + map + "/move/" + des))
                 .POST(HttpRequest.BodyPublishers.ofString(""))
                 .build();
 
-        response(client, request);
+        return response(client, request);
     }
 
-    public void upload(String uid, String map, String FileName) {
+    public Object[] upload(String uid, String map, String FileName) throws FileNotFoundException {
         HttpClient client = HttpClient.newHttpClient();
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://tesla.iem.pw.edu.pl:4444/" + uid + "/" + map + "/upload"))
-                    .POST(HttpRequest.BodyPublishers.ofFile(Paths.get(FileName)))
-                    .build();
-            response(client, request);
-        } catch (FileNotFoundException ignored) {
-        }
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://tesla.iem.pw.edu.pl:4444/" + uid + "/" + map + "/upload"))
+                .POST(HttpRequest.BodyPublishers.ofFile(Paths.get(FileName)))
+                .build();
+        return response(client, request);
 
     }
 
-    private void response(HttpClient client, HttpRequest request) {
+    public Object[] reset(String uid, String map){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://tesla.iem.pw.edu.pl:4444/" + uid + "/" + map + "/reset"))
+                .POST(HttpRequest.BodyPublishers.ofString(""))
+                .build();
+        return response(client, request);
+
+    }
+
+    public Object[] moves(String uid, String map) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://tesla.iem.pw.edu.pl:4444/" + uid + "/" + map + "/moves"))
+                .build();
+
+        return response(client, request);
+    }
+
+    public Object[] startPoss(String uid, String map) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://tesla.iem.pw.edu.pl:4444/" + uid + "/" + map + "/startposition"))
+                .build();
+
+        return response(client, request);
+    }
+
+    public Object[] size(String uid, String map) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://tesla.iem.pw.edu.pl:4444/" + uid + "/" + map + "/size"))
+                .build();
+
+        return response(client, request);
+    }
+
+
+
+    private Object[] response(HttpClient client, HttpRequest request) {
         HttpResponse<String> response = null;
         try {
             response = client.send(request,
@@ -49,15 +85,17 @@ public class RequestHandler {
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
-        System.out.println(response.statusCode()); //200
-        System.out.println(response.body()); // Moved successfully
+
+        return new Object[]{response.statusCode(), response.body()};
     }
+
+
 
 
     public static void main(String[] args) {
         RequestHandler r = new RequestHandler();
-        r.getPoss("bd5f6f92" , "1");
-        r.move("bd5f6f92", "1" , "left");
-        r.getPoss("bd5f6f92" , "1");
+        r.getPoss("bd5f6f92", "1");
+        r.move("bd5f6f92", "1", "left");
+        r.getPoss("bd5f6f92", "1");
     }
 }
